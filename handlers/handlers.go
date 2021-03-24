06c91402371cd65322b0b56bc8bb3bb97eb4098c
noisersup <patekiapl@gmail.com>
2021-03-24 13:18:07 +0100

    GET request handling

---
+++ handlers/handlers.go
@@ -0,0 +1,11 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+func GetTimestamp(w http.ResponseWriter, r *http.Request) {
+	log.Print("GET!") //TODO: remove
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+	"github.com/noisersup/dashboard-backend-pomodoro/handlers"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/pomodoro", handlers.GetTimestamp).Methods("GET")
+
+	log.Printf("Starting http server on port :8000...")
+	log.Fatal(http.ListenAndServe(":8005", r))
 }
