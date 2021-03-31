1fd76a97948a7b8a7eb15630bbb752ce33c40f9c
noisersup <patekiapl@gmail.com>
2021-03-31 23:35:26 +0200

    :construction: Return 0 when database is empty

--- database/database.go
+++ database/database.go
@@ -53,11 +53,12 @@ func (db *Database) GetTimestamp() (int, error) {
 	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	cur.Next(ctx)
 	var resp models.TimestampDb
-	err = cur.Decode(&resp)
-	if err != nil {return 0,err}
 
+	for cur.Next(ctx){
+		err = cur.Decode(&resp)
+		if err != nil {return 0,err}
+	}
 	if err := cur.Err(); err != nil{
 		return 0,err
 	}
